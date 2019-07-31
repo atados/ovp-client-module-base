1b9b745ce97e65e5270acf63ec061f62a6850fb2
Vinicius Pacheco Furtado <vin175pacheco@gmail.com>
2019-07-31 16:40:17 -0300

    Fix lint issues

--- lib/intl/index.tsx
+++ lib/intl/index.tsx
@@ -1,11 +1,11 @@
-import useIntl from '~/hooks/use-intl'
 import { InjectedIntlProps } from 'react-intl'
+import useIntl from '~/hooks/use-intl'
 
 export function withIntl<Props extends InjectedIntlProps>(
   Component: React.ComponentType<Props>,
 ): React.FC<Omit<Props, keyof InjectedIntlProps>> {
   return props => {
     const intl = useIntl()
-    return <Component {...(props as Props)} intl={intl} />
+    return <Component {...props as Props} intl={intl} />
   }
 }
