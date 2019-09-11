89831909df2c53ec6653fd6772dc1b0e53d06b65
kcotias <kaianexperia@gmail.com>
2019-09-11 05:08:53 -0300

    add intl to searchfilters

--- components/SearchFilters/SearchFilters.tsx
+++ components/SearchFilters/SearchFilters.tsx
@@ -1,22 +1,24 @@
-import isEqual from 'fast-deep-equal'
-import Router from 'next/router'
-import queryString from 'query-string'
-import React from 'react'
-import styled from 'styled-components'
-import { Page, PageAs } from '~/common'
-import { channel } from '~/common/constants'
-import CausesFilter from '~/components/CausesFilter'
-import DisponibilityFilter from '~/components/DisponibilityFilter'
-import SkillsFilter from '~/components/SkillsFilter'
-import ToggleSwitch from '~/components/ToggleSwitch'
+import isEqual from "fast-deep-equal";
+import Router from "next/router";
+import queryString from "query-string";
+import React from "react";
+import styled from "styled-components";
+import { Page, PageAs } from "~/common";
+import { channel } from "~/common/constants";
+import CausesFilter from "~/components/CausesFilter";
+import DisponibilityFilter from "~/components/DisponibilityFilter";
+import SkillsFilter from "~/components/SkillsFilter";
+import ToggleSwitch from "~/components/ToggleSwitch";
 import {
   BaseFilters,
   DisponibilityFilterValue,
   mapFiltersToQueryObject,
-  SearchType,
-} from '~/redux/ducks/search'
-import Icon from '../Icon'
-import { SearchFilterButton } from './SearchFilter'
+  SearchType
+} from "~/redux/ducks/search";
+import Icon from "../Icon";
+import { SearchFilterButton } from "./SearchFilter";
+import { InjectedIntlProps, defineMessages } from "react-intl";
+import { withIntl } from "~/lib/intl";
 
 const Container = styled.div`
   .direction-right .dropdown-menu {
@@ -28,7 +30,7 @@ const Container = styled.div`
     left: auto;
     right: 0;
   }
-`
+`;
 
 const MapToggleSwitchLabel = styled.label`
   user-select: none;
@@ -38,173 +40,179 @@ const MapToggleSwitchLabel = styled.label`
     vertical-align: middle;
     margin-left: 10px;
   }
-`
+`;
+
+const { VAGAS_A_DISTANCIA, ENDERECO, MOSTRAR_MAPA } = defineMessages({
+  VAGAS_A_DISTANCIA: {
+    id: "VAGAS_A_DISTANCIA",
+    defaultMessage: "Somente vagas à distância"
+  },
+  ENDERECO: {
+    id: "ENDERECO",
+    defaultMessage: "Endereço"
+  },
+  MOSTRAR_MAPA: {
+    id: "MOSTRAR_MAPA",
+    defaultMessage: "Mostrar mapa"
+  }
+});
 
 interface SearchFiltersProps {
-  readonly value: BaseFilters
-  readonly searchType?: SearchType
-  readonly mapToggleChecked?: boolean
-  readonly onOpenStateChange?: (open: boolean) => void
-  readonly onMapSwitchChange: (visible: boolean) => any
+  readonly value: BaseFilters;
+  readonly searchType?: SearchType;
+  readonly mapToggleChecked?: boolean;
+  readonly onOpenStateChange?: (open: boolean) => void;
+  readonly onMapSwitchChange: (visible: boolean) => any;
 }
 
 interface SearchFiltersState {
-  value: BaseFilters
-  dirty: boolean
+  value: BaseFilters;
+  dirty: boolean;
 }
 
 class SearchFilters extends React.Component<
-  SearchFiltersProps,
+  SearchFiltersProps & InjectedIntlProps,
   SearchFiltersState
 > {
   public static getDerivedStateFromProps(
     props: SearchFiltersProps,
-    state?: SearchFiltersState,
+    state?: SearchFiltersState
   ): SearchFiltersState {
     return {
       dirty: state ? state.dirty : false,
-      value: state ? (!state.dirty ? props.value : state.value) : props.value,
-    }
+      value: state ? (!state.dirty ? props.value : state.value) : props.value
+    };
   }
 
   private fields: {
     [fieldName: string]: {
-      open?: boolean
-      onOpenStateChange: (open: boolean) => void
-    }
-  }
+      open?: boolean;
+      onOpenStateChange: (open: boolean) => void;
+    };
+  };
 
   constructor(props) {
-    super(props)
+    super(props);
 
-    this.state = SearchFilters.getDerivedStateFromProps(props)
+    this.state = SearchFilters.getDerivedStateFromProps(props);
     this.fields = {
       causes: {
-        onOpenStateChange: this.handleFilterOpenStateChange.bind(
-          this,
-          'causes',
-        ),
+        onOpenStateChange: this.handleFilterOpenStateChange.bind(this, "causes")
       },
       skills: {
-        onOpenStateChange: this.handleFilterOpenStateChange.bind(
-          this,
-          'skills',
-        ),
+        onOpenStateChange: this.handleFilterOpenStateChange.bind(this, "skills")
       },
       disponibility: {
         onOpenStateChange: this.handleFilterOpenStateChange.bind(
           this,
-          'disponibility',
-        ),
+          "disponibility"
+        )
       },
       remote: {
-        onOpenStateChange: this.handleFilterOpenStateChange.bind(
-          this,
-          'remote',
-        ),
-      },
-    }
+        onOpenStateChange: this.handleFilterOpenStateChange.bind(this, "remote")
+      }
+    };
   }
 
   public handleFilterOpenStateChange = (fieldName: string, open: boolean) => {
-    const { onOpenStateChange } = this.props
+    const { onOpenStateChange } = this.props;
     if (onOpenStateChange) {
-      this.fields[fieldName].open = open
+      this.fields[fieldName].open = open;
       const isOneOpen = Object.keys(this.fields).some(name =>
-        Boolean(this.fields[name].open),
-      )
+        Boolean(this.fields[name].open)
+      );
 
-      onOpenStateChange(isOneOpen)
+      onOpenStateChange(isOneOpen);
     }
-  }
+  };
 
   public handleMapToggleSwitchChange = (
-    event: React.ChangeEvent<HTMLInputElement>,
+    event: React.ChangeEvent<HTMLInputElement>
   ) => {
-    const { onMapSwitchChange } = this.props
-    const { checked } = event.target
+    const { onMapSwitchChange } = this.props;
+    const { checked } = event.target;
 
-    onMapSwitchChange(checked)
-  }
+    onMapSwitchChange(checked);
+  };
 
   public handleDisponibilityFilterChange = (
-    newValue: DisponibilityFilterValue,
+    newValue: DisponibilityFilterValue
   ) => {
     this.setState({
       dirty: true,
       value: {
         ...this.state.value,
-        disponibility: newValue,
-      },
-    })
-  }
+        disponibility: newValue
+      }
+    });
+  };
 
   public handleRemoteFilterChange = (newValue: boolean) => {
     this.setState({
       dirty: true,
       value: {
         ...this.state.value,
-        remoteOnly: newValue,
-      },
-    })
-  }
+        remoteOnly: newValue
+      }
+    });
+  };
 
   public handleCausesFilterChange = (newValue: number[]) => {
     this.setState({
       dirty: true,
       value: {
         ...this.state.value,
-        causes: newValue,
-      },
-    })
-  }
+        causes: newValue
+      }
+    });
+  };
 
   public handleSkillsFilterChange = (newValue: number[]) => {
     this.setState({
       dirty: true,
       value: {
         ...this.state.value,
-        skills: newValue,
-      },
-    })
-  }
+        skills: newValue
+      }
+    });
+  };
 
   public commit = () => {
-    const { searchType } = this.props
-    const { value } = this.state
+    const { searchType } = this.props;
+    const { value } = this.state;
 
     if (isEqual(value, this.props.value)) {
-      return
+      return;
     }
 
     const query = queryString.stringify({
       ...mapFiltersToQueryObject(value),
-      searchType,
-    })
+      searchType
+    });
 
-    let pageName: keyof typeof Page = 'Search'
+    let pageName: keyof typeof Page = "Search";
 
     if (searchType === SearchType.Projects) {
-      pageName = 'SearchProjects'
+      pageName = "SearchProjects";
     } else if (searchType === SearchType.Organizations) {
-      pageName = 'SearchOrganizations'
+      pageName = "SearchOrganizations";
     }
 
-    Router.push(`${Page[pageName]}?${query}`, `${PageAs[pageName]()}?${query}`)
-    this.setState({ dirty: false })
-  }
+    Router.push(`${Page[pageName]}?${query}`, `${PageAs[pageName]()}?${query}`);
+    this.setState({ dirty: false });
+  };
 
   public handleAddressFilterClick = () => {
-    const element = document.getElementById('search-form-input')
+    const element = document.getElementById("search-form-input");
 
     if (element) {
-      element.focus()
+      element.focus();
     }
-  }
+  };
 
   public render() {
-    const { searchType, mapToggleChecked } = this.props
-    const { value } = this.state
+    const { searchType, mapToggleChecked, intl } = this.props;
+    const { value } = this.state;
 
     return (
       <Container className="flex">
@@ -215,10 +223,10 @@ class SearchFilters extends React.Component<
         >
           <Icon name="place" className="mr-1" />
           {value.remoteOnly
-            ? 'Somente vagas à distância'
+            ? intl.formatMessage(VAGAS_A_DISTANCIA)
             : value.address
             ? value.address.description
-            : 'Endereço'}
+            : intl.formatMessage(ENDERECO)}
         </SearchFilterButton>
         <CausesFilter
           value={value.causes}
@@ -249,7 +257,9 @@ class SearchFilters extends React.Component<
         {channel.config.maps.key && searchType !== SearchType.Any && (
           <div className="hidden md:block">
             <MapToggleSwitchLabel htmlFor="filters-show-map-toggler">
-              <span className="ts-small">Mostrar Mapa</span>
+              <span className="ts-small">
+                {intl.formatMessage(MOSTRAR_MAPA)}
+              </span>
               <ToggleSwitch
                 className="input-map-switch"
                 id="filters-show-map-toggler"
@@ -261,8 +271,8 @@ class SearchFilters extends React.Component<
           </div>
         )}
       </Container>
-    )
+    );
   }
 }
 
-export default SearchFilters
+export default SearchFilters;
