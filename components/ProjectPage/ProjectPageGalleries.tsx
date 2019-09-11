89a32b80db49bb90374bf81a772c898ceffd6ad4
kcotias <kaianexperia@gmail.com>
2019-09-11 04:33:43 -0300

    add intl to pagegalleries

--- components/ProjectPage/ProjectPageGalleries.tsx
+++ components/ProjectPage/ProjectPageGalleries.tsx
@@ -1,9 +1,11 @@
-import React, { useMemo } from 'react'
-import styled from 'styled-components'
-import { ImageDict } from '~/common/channel'
-import { Project } from '~/redux/ducks/project'
-import Gallery from '../Gallery'
-import { useModal } from '../Modal'
+import React, { useMemo } from "react";
+import styled from "styled-components";
+import { ImageDict } from "~/common/channel";
+import { Project } from "~/redux/ducks/project";
+import Gallery from "../Gallery";
+import { useModal } from "../Modal";
+import { defineMessages } from "react-intl";
+import useIntl from "~/hooks/use-intl";
 
 const Photos = styled.div`
   &.row {
@@ -13,11 +15,11 @@ const Photos = styled.div`
   .col-4 {
     padding: 0 5px;
   }
-`
+`;
 
 const Photo = styled.figure`
   margin-bottom: 10px;
-`
+`;
 
 const PhotoImage = styled.div`
   background: rgba(0, 0, 0, 0.05);
@@ -25,47 +27,56 @@ const PhotoImage = styled.div`
   background-position: center;
   cursor: pointer;
   box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.1);
-`
+`;
 
 interface ProjectPageGalleriesProps {
-  readonly project: Project
+  readonly project: Project;
 }
 
-const ProjectPageGalleries: React.FC<ProjectPageGalleriesProps> = ({
-  project,
-}) => {
-  const openGallery = useModal({
-    id: 'Gallery',
-    component: Gallery,
-    cardClassName: 'no-animation',
-  })
-  const images = useMemo(() => {
-    const imagesList: ImageDict[] = []
-    project.galleries.forEach(gallery => {
-      imagesList.push(...gallery.images)
-    })
+const { FOTOS } = defineMessages({
+  FOTOS: {
+    id: "FOTOS",
+    defaultMessage: "Fotos"
+  }
+});
 
-    return imagesList
-  }, [project && project.galleries])
+const ProjectPageGalleries: React.FC<ProjectPageGalleriesProps> = ({
+  project
+}) => {
+  const intl = useIntl();
+
+  const openGallery = useModal({
+    id: "Gallery",
+    component: Gallery,
+    cardClassName: "no-animation"
+  });
+  const images = useMemo(() => {
+    const imagesList: ImageDict[] = [];
+    project.galleries.forEach(gallery => {
+      imagesList.push(...gallery.images);
+    });
+
+    return imagesList;
+  }, [project && project.galleries]);
 
   if (images.length === 0) {
-    return null
+    return null;
   }
 
   return (
     <>
-      <h4 className="mb-4">Fotos</h4>
+      <h4 className="mb-4">{intl.formatMessage(FOTOS)}</h4>
       <Photos className="row">
         {images.map(image => (
           <div key={image.id} className="col-4">
             <Photo className="ratio">
-              <span className="ratio-fill" style={{ paddingTop: '100%' }} />
+              <span className="ratio-fill" style={{ paddingTop: "100%" }} />
               <PhotoImage
                 className="ratio-body"
                 style={
                   image && {
                     backgroundImage: `url('${image.image_url ||
-                      image.image_medium}')`,
+                      image.image_medium}')`
                   }
                 }
                 onClick={() =>
@@ -78,9 +89,9 @@ const ProjectPageGalleries: React.FC<ProjectPageGalleriesProps> = ({
       </Photos>
       <hr className="mt-4 mb-4" />
     </>
-  )
-}
+  );
+};
 
-ProjectPageGalleries.displayName = 'ProjectPageGalleries'
+ProjectPageGalleries.displayName = "ProjectPageGalleries";
 
-export default React.memo(ProjectPageGalleries)
+export default React.memo(ProjectPageGalleries);
