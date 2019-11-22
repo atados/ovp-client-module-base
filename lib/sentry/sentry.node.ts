f047405ca20c401966743bd239f0400e528497ad
Vinicius Pacheco Furtado <vin175pacheco@gmail.com>
2019-11-21 21:45:22 -0300

    Add sentry for server

---
+++ lib/sentry/sentry.node.ts
@@ -0,0 +1,26 @@
+import * as Sentry from '@sentry/node'
+import { Config } from '~/base/common'
+import {
+  NOW_GITHUB_COMMIT_SHA,
+  NOW_GITHUB_COMMIT_DIRTY,
+} from '~/common/constants'
+export function setupErrorMonitoringOnServer() {
+  // Only run Sentry on production
+  if (Config.sentry) {
+    Sentry.init({
+      ...(Config.sentry as any),
+      environment: `${process.env.NODE_ENV || 'development'}${
+        NOW_GITHUB_COMMIT_SHA
+          ? `_now_${NOW_GITHUB_COMMIT_SHA}${
+              NOW_GITHUB_COMMIT_DIRTY === 'true' ? '_dirty' : ''
+            }`
+          : ''
+      }`,
+    })
+  }
+}
+
+export function reportNodeError(error: any) {
+  console.log(error)
+  Sentry.captureException(error)
+}
