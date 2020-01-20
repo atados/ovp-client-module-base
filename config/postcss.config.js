0490356bb825dc750b71a8ae70e502d47e03d227
Vinicius Pacheco Furtado <vin175pacheco@gmail.com>
2020-01-20 18:08:16 -0300

    Add postcss.config + Fix FAQLayout margin on brand

---
+++ config/postcss.config.js
@@ -0,0 +1,37 @@
+const { screens } = require('tailwindcss/defaultTheme')
+const cssVariables = require('../internals/css-variables')
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project
+  content: [
+    './base/**/*.ts',
+    './channel/**/*.ts',
+    './base/**/*.tsx',
+    './channel/**/*.tsx',
+    // etc.
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+})
+
+const customMedia = {}
+
+Object.keys(screens).forEach(screenName => {
+  customMedia[`--min-${screenName}`] = `(min-width: ${screens[screenName]})`
+  customMedia[`--max-${screenName}`] = `(max-width: ${screens[screenName]})`
+})
+
+const useTailwind = Boolean(process.env.TAILWIND)
+const prod = process.env.NODE_ENV === 'production'
+module.exports = {
+  plugins: [
+    ...(useTailwind ? [require('postcss-import'), require('tailwindcss')] : []),
+    require('postcss-nested'),
+    require('postcss-custom-media')({
+      importFrom: [{ customMedia }],
+    }),
+    require('postcss-css-variables')({ variables: cssVariables }),
+    require('autoprefixer'),
+    ...(prod ? [purgecss, require('cssnano')] : []),
+  ],
+}
