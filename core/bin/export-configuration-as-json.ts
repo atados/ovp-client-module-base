bc4033c950a58c0c4db6f86334e824a896076777
Vinicius Pacheco Furtado <vin175pacheco@gmail.com>
2020-01-24 15:20:01 -0300

    Fix: Export configuration fails if channel dir doesn't exit

--- core/bin/export-configuration-as-json.ts
+++ core/bin/export-configuration-as-json.ts
@@ -1,11 +1,14 @@
 import { Config } from '~/common'
 import fs from 'fs'
+import legacyMkdirp from 'mkdirp'
 import { promisify } from 'util'
 import * as path from 'path'
 
 const writeFile = promisify(fs.writeFile)
+const mkdirp = promisify(legacyMkdirp)
 
 async function main() {
+  await mkdirp(path.resolve('channel'))
   await writeFile(
     path.resolve('channel', 'app.json'),
     JSON.stringify(Config, null, 2),
