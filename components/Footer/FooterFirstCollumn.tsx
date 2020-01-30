4f2581192bad67a4f34f6c850bf037b50c54fdf5
Peterson Passos <peterson.jfp@gmail.com>
2020-01-30 15:56:30 -0300

    Decouple: FooterBottom and FooterFirstColumn

---
+++ components/Footer/FooterFirstCollumn.tsx
@@ -0,0 +1,36 @@
+import LanguageDropdown from '~/components/LanguageDropdown/LanguageDropdown'
+import FooterNav from '~/components/Footer/FooterNav'
+import { Config, GlobalMessages } from '~/common'
+import { useIntl } from 'react-intl'
+import Link from 'next/link'
+import React from 'react'
+
+const FooterFirstCollumn = () => {
+  const intl = useIntl()
+
+  return (
+    <FooterNav title={intl.formatMessage(GlobalMessages.appName)}>
+      {Config.footer.links.length === 0 && (
+        <span>{intl.formatMessage(GlobalMessages.appDescription)}</span>
+      )}
+      {Config.footer.links.map(link => (
+        <li key={link.href + link.as} className="nav-item">
+          {link.as ? (
+            <Link href={link.href} as={link.as}>
+              <a className="nav-link">{link.label}</a>
+            </Link>
+          ) : (
+            <a href={link.href} target="__blank" className="nav-link">
+              {link.label}
+            </a>
+          )}
+        </li>
+      ))}
+      <li className="nav-item">
+        <LanguageDropdown />
+      </li>
+    </FooterNav>
+  )
+}
+
+export default FooterFirstCollumn
